a7a01c2f85c6631b3e646a4cc982f1ebfb28d745
Chris <christoffer.radenholt@hotmail.se>
2023-02-09 13:33:23 +0100

    Adding test class using Mockito and testing findAll method

---
+++ src/test/java/com/example/EmployeeManagerTestWithMockito.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeManagerTestWithMockito {
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @Mock
+    private BankService bankService;
+
+    private EmployeeManager employeeManager;
+
+
+    @BeforeEach
+    public void initializeEmployeeManager(){
+        this.employeeManager = new EmployeeManager(employeeRepository,bankService);
+    }
+
+    @Test
+    public void testShouldCallFindAllMethod(){
+
+        when(employeeRepository.findAll()).thenReturn(List.of(new Employee("1", 17000)));
+
+        employeeManager.payEmployees();
+
+        verify(employeeRepository, atLeastOnce()).findAll();
+
+
+    }
+
+
+
+
+
+}
