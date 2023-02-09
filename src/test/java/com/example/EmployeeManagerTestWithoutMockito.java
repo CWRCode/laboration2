530ae771db8fc0eca4531e9181f0d84a4ee247ee
Chris <christoffer.radenholt@hotmail.se>
2023-02-09 13:54:19 +0100

    Adding spy for BankService, implements first test for FindAll method

--- src/test/java/com/example/EmployeeManagerTestWithoutMockito.java
+++ src/test/java/com/example/EmployeeManagerTestWithoutMockito.java
@@ -1,7 +1,21 @@
 package com.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class EmployeeManagerTestWithoutMockito {
 
+    @Test
+    public void TestMethodFindAllShouldReturnOneEmployee(){
+
+        Employee employee = new Employee("1", 15000);
+
+        EmployeeRepositoryStub EmployeeRepositoryStub = new EmployeeRepositoryStub();
+
+        Assertions.assertEquals(EmployeeRepositoryStub.findAll(), List.of(employee));
+    }
 
 
 
